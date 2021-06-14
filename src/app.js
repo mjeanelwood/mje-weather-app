dc00775e4ecda36c28aabf8f9a8484d21fcd614b
mjeanelwood <85648554+mjeanelwood@users.noreply.github.com>
2021-06-14 12:24:07 -0700

    Created search engine

--- src/app.js
+++ src/app.js
@@ -45,8 +45,19 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "da8bebb3e3f08446cad530447d5543ae";
-let city = "Boise";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "da8bebb3e3f08446cad530447d5543ae";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Los Angeles");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
